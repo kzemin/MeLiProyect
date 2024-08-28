0f646f406441396db03709a93075606967f14767
Facundo Kzemin Paez <facupaezk@gmail.com>
2024-08-28 16:34:38 -0300

    Initial Commit

---
+++ MeLiProyect/MeLiProyectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeLiProyectApp.swift
+//  MeLiProyect
+//
+//  Created by Facundo Kzemin on 28/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MeLiProyectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
